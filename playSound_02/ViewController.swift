0b13b7a48af95a13a156ae367bd59467ae62fff4
MikeWu <MikeWu@mikewude-macbook-pro.local>
2016-06-11 22:56:19 +0800

    Play internet sound

--- playSound_02/ViewController.swift
+++ playSound_02/ViewController.swift
@@ -1,25 +1,29 @@
-//
-//  ViewController.swift
-//  playSound_02
-//
-//  Created by MikeWu on 2016/6/11.
-//  Copyright © 2016年 MSMedia. All rights reserved.
-//
-
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
+    
+    // 設定變數為MPMoviePlayerController()的函式
+    var audioPlayer = MPMoviePlayerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        do {
+            // 設定變數儲存音效網路路徑
+            let filePath = "https://a64bd602f65d8c0153dd418797f9dc197bc2a865.googledrive.com/host/0B50Vmg6QH_TMdFJTc3JlN0M1RGs"
+            // 將音效變數與路徑做關聯
+            audioPlayer.contentURL = NSURL(string: filePath)
+        } catch {
+            print("Error")
+        }
+        audioPlayer.prepareToPlay()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // 按鈕設定
+    @IBAction func playButton(sender: AnyObject) {
+        audioPlayer.play()
     }
-
 
 }
 
